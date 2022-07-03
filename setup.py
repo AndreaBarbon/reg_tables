3e3c81df5ed2fee39c25f0fcfcbe7318db641727
AndreaBarbon <andrea.3arbon@gmail.com>
2022-07-03 22:44:17 +0200

    0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh: long_description = fh.read()
 
 setuptools.setup(
     name="reg_tables",                     
-    version="0.1.1",
+    version="0.1.2",
     author="Andrea Barbon, Kirill Kazakov",                     
     author_email='andrea.3arbon@gmail.com',
     url = 'https://www.abarbon.com',
